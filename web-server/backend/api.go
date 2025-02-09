0687dec10240c8b1c6c67ccb3d16a545e79a90b3
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2025-02-09 22:39:52 +0100

    Rename HTTP handler functions for clarity and consistency

--- web-server/backend/api.go
+++ web-server/backend/api.go
@@ -22,7 +22,7 @@ func writeResponse(w http.ResponseWriter, status int, value any) error {
 	return json.NewEncoder(w).Encode(value)
 }
 
-func makeHTTPHandleFunc(fn apiFunc) http.HandlerFunc {
+func createHttpHandler(fn apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := fn(w, r); err != nil {
 			//TODO: check error from handlers and return appropriate status code
@@ -39,14 +39,14 @@ func NewApiServer(listenAddr string) *ApiServer {
 
 func (server *ApiServer) Run() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /users", makeHTTPHandleFunc(server.getAllUsers))
+	mux.HandleFunc("GET /users", createHttpHandler(server.handleGetAllUsers))
 
 	log.Println("Starting server on port", server.listenAddr)
 
 	http.ListenAndServe(server.listenAddr, mux)
 }
 
-func (server *ApiServer) getAllUsers(w http.ResponseWriter, r *http.Request) error {
+func (server *ApiServer) handleGetAllUsers(w http.ResponseWriter, r *http.Request) error {
 	users := GenerateDummyUsers()
 	return writeResponse(w, http.StatusOK, users)
 }
