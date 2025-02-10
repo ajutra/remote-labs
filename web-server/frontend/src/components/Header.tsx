d3224841a5d8e26a3b2f36d4d5b988c39a8a79ce
Julen Rodriguez <122809250+JulenRM10@users.noreply.github.com>
2025-02-10 22:04:08 +0100

    Refactor Header component layout for improved responsiveness and add highlight color variable in Tailwind configuration

--- web-server/frontend/src/components/Header.tsx
+++ web-server/frontend/src/components/Header.tsx
@@ -4,18 +4,20 @@ import ModeToggle from '@/components/mode-toggle'
 
 const Header: React.FC = () => {
   return (
-    <header className="flex flex-wrap items-center justify-between bg-card p-4 text-card-foreground">
-      <div className="flex items-center">
+    <header className="flex flex-wrap items-center justify-between space-y-3 bg-card p-4 text-card-foreground md:space-y-0">
+      <div className="flex w-full justify-center md:w-auto md:justify-start">
         <img
           src="/src/assets/logo.png"
           alt="Logo"
           className="h-12 w-auto object-contain"
         />
       </div>
-      <div className="order-2 flex flex-1 justify-center md:order-1">
-        <span className="whitespace-nowrap text-xl font-bold">Remote Labs</span>
+      <div className="order-1 flex w-full justify-center md:order-1 md:w-auto md:justify-center">
+        <span className="text-highlight whitespace-nowrap text-2xl font-bold">
+          Remote Labs
+        </span>
       </div>
-      <div className="order-1 flex items-center space-x-4 md:order-2">
+      <div className="order-2 flex w-full justify-center space-x-4 md:order-2 md:w-auto md:justify-end">
         <LanguageSelector />
         <ModeToggle />
       </div>
