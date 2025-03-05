3943a99ba3c10583bd8fda4ceb136da8683d4457
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2025-03-05 21:17:38 +0100

    Add API for server agent

---
+++ server-agent/api.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type ApiServer struct {
+	listenAddr string
+	service    Service
+}
+
+type ApiError struct {
+	Error string `json:"error"`
+}
+
+func writeResponse(w http.ResponseWriter, status int, value any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(value)
+}
+
+func createHttpHandler(fn apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			var status int
+			if httpErr, ok := err.(*HttpError); ok {
+				status = httpErr.StatusCode
+			} else {
+				status = http.StatusInternalServerError
+			}
+			writeResponse(w, status, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+func NewApiServer(listenAddr string, service Service) *ApiServer {
+	return &ApiServer{
+		listenAddr: listenAddr,
+		service:    service,
+	}
+}
+
+func (server *ApiServer) Run() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /vms/clone", createHttpHandler(server.handleCloneVM))
+	mux.HandleFunc("DELETE /vms/delete/{vmName}", createHttpHandler(server.handleDeleteVM))
+	mux.HandleFunc("POST /vms/start/{vmName}", createHttpHandler(server.handleStartVM))
+	mux.HandleFunc("POST /vms/stop/{vmName}", createHttpHandler(server.handleStopVM))
+	mux.HandleFunc("POST /vms/restart/{vmName}", createHttpHandler(server.handleRestartVM))
+	mux.HandleFunc("POST /vms/force-stop/{vmName}", createHttpHandler(server.handleForceStopVM))
+	mux.HandleFunc("GET /vms/status", createHttpHandler(server.handleListVMsStatus))
+
+	log.Println("Starting server on", server.listenAddr)
+
+	if err := http.ListenAndServe(server.listenAddr, mux); err != nil {
+		log.Fatal("Error starting server:", err)
+	}
+}
