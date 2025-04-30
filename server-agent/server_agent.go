aba4281f57ad722a16d2377cbd8dd021f48f56a3
Aitor Juanola <119131537+ajutra@users.noreply.github.com>
2025-04-30 18:14:39 +0200

    refactor: improve template and instance management
    - Rename VM-related methods to use 'Instance' terminology for consistency
    - Add new structs for cloud-init metadata and user data
    - Implement template metadata file creation with cloud-init support
    - Add disk image creation functionality for templates
    - Remove deprecated VM cloning functionality
    - Add TODOs for future implementation of instance operations
    - Improve error handling and logging throughout the codebase
    
    This commit standardizes the terminology from 'VM' to 'Instance' and
    adds support for cloud-init configuration in templates. The changes
    include better separation of concerns with new structs for metadata
    and user data, as well as improved disk image management.

--- server-agent/server_agent.go
+++ server-agent/server_agent.go
@@ -1,47 +1,132 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+	"text/template"
 )
 
+//go:embed templates/*.tmpl
+var templateFS embed.FS
+
 type ServerAgent interface {
-	CloneVM(baseVmId string, newVmId string) error
-	DeleteVM(vmId string) error
-	StartVM(vmId string) error
-	StopVM(vmId string) error
-	RestartVM(vmId string) error
-	ForceStopVM(vmId string) error
-	ListVMsStatus() ([]ListVMsStatusResponse, error)
+	ListBaseImages() ([]ListBaseImagesResponse, error)
+	DefineTemplate(request DefineTemplateRequest) error
+	CreateInstance(request CreateInstanceRequest) error
+	DeleteVm(vmId string) error
+	StartInstance(instanceId string) error
+	StopInstance(instanceId string) error
+	RestartInstance(instanceId string) error
+	ListInstancesStatus() ([]ListInstancesStatusResponse, error)
 }
 
-type ServerAgentImpl struct{}
+type ServerAgentImpl struct {
+	vmsStoragePath      string
+	cloudInitImagesPath string
+}
 
-func (agent *ServerAgentImpl) CloneVM(baseVmId string, newVmId string) error {
-	log.Printf("Cloning VM '%s' to '%s'...", baseVmId, newVmId)
+type CloudInitMetadata struct {
+	InstanceId    string
+	LocalHostname string
+}
+
+type CloudInitUserData struct {
+	Username      string
+	Password      string
+	PublicSshKeys []string
+}
+
+type CreateDiskImageData struct {
+	sourceVmId   string
+	sourceIsBase bool
+	SizeMB       int
+}
+
+func (agent *ServerAgentImpl) ListBaseImages() ([]ListBaseImagesResponse, error) {
+	// TODO: Figure out how to rename the base images to it's id once it's generated
+	log.Printf("Getting base images...")
 
 	cmd := exec.Command(
-		"virt-clone",
-		"--connect", "qemu:///system",
-		"--original", baseVmId,
-		"--name", newVmId,
-		"--file", "/var/lib/libvirt/images/"+newVmId+".qcow2",
+		"ls",
+		agent.cloudInitImagesPath,
 	)
 
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
-		return logAndReturnError("Error cloning VM '"+baseVmId+"' to '"+newVmId+"': ", string(output))
+		return nil, logAndReturnError("Error listing base VMs: ", string(output))
 	}
 
-	log.Printf("Cloned VM '%s' to '%s' successfully!", baseVmId, newVmId)
+	files := strings.Split(string(output), "\n")
+
+	// Remove empty strings from the list
+	files = files[:len(files)-1]
+
+	log.Printf("Base VMs: %v", files)
+
+	return toListBaseImagesResponse(files), nil
+}
+
+func (agent *ServerAgentImpl) DefineTemplate(request DefineTemplateRequest) error {
+	log.Printf("Defining template...")
+
+	dirPath := agent.vmsStoragePath + "/" + request.TemplateId
+
+	log.Printf("Creating template directory...")
+
+	if err := createDir(dirPath); err != nil {
+		return logAndReturnError("Error creating template directory: ", err.Error())
+	}
+
+	log.Printf("Template directory created successfully!")
+
+	log.Printf("Creating metadata files...")
+
+	if err := agent.createTemplateMetadataFiles(dirPath, request); err != nil {
+		return err
+	}
+
+	log.Printf("Metadata files created successfully!")
+
+	log.Printf("Creating template disk image...")
+	createDiskImageData := CreateDiskImageData{
+		sourceVmId:   request.SourceInstanceId,
+		sourceIsBase: false,
+		SizeMB:       request.SizeMB,
+	}
+
+	if err := agent.createDiskImage(dirPath, createDiskImageData); err != nil {
+		return err
+	}
+
+	log.Printf("Template disk image created successfully!")
+
+	// TODO: Create the vm using virt-install
+
+	// TODO: Shutdown the vm
+
+	// TODO: Remove the backing file from the template disk image using qemu-img rebase
+
+	log.Printf("Template '%s' defined successfully!", request.TemplateId)
 
 	return nil
 }
 
-func (agent *ServerAgentImpl) DeleteVM(vmId string) error {
+func (agent *ServerAgentImpl) CreateInstance(request CreateInstanceRequest) error {
+	log.Printf("Creating instance '%s'...", request.InstanceId)
+
+	// TODO: Implement
+
+	return nil
+}
+
+// TODO: Implement
+func (agent *ServerAgentImpl) DeleteVm(vmId string) error {
 	log.Printf("Deleting VM '%s'...", vmId)
 
 	cmd := exec.Command(
@@ -59,59 +144,64 @@ func (agent *ServerAgentImpl) DeleteVM(vmId string) error {
 	return nil
 }
 
-func (agent *ServerAgentImpl) StartVM(vmId string) error {
-	log.Printf("Starting VM '%s'...", vmId)
+// TODO: Implement
+func (agent *ServerAgentImpl) StartInstance(instanceId string) error {
+	log.Printf("Starting instance '%s'...", instanceId)
 
 	cmd := exec.Command(
-		"virsh", "start", vmId,
+		"virsh", "start", instanceId,
 	)
 
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
-		return logAndReturnError("Error starting VM '"+vmId+"': ", string(output))
+		return logAndReturnError("Error starting instance '"+instanceId+"': ", string(output))
 	}
 
-	log.Printf("Started VM '%s' successfully!", vmId)
+	log.Printf("Started instance '%s' successfully!", instanceId)
 
 	return nil
 }
 
-func (agent *ServerAgentImpl) StopVM(vmId string) error {
-	log.Printf("Stopping VM '%s'...", vmId)
+// TODO: Implement
+func (agent *ServerAgentImpl) StopInstance(instanceId string) error {
+	log.Printf("Stopping instance '%s'...", instanceId)
 
 	cmd := exec.Command(
-		"virsh", "shutdown", vmId,
+		"virsh", "shutdown", instanceId,
 	)
 
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
-		return logAndReturnError("Error stopping VM '"+vmId+"': ", string(output))
+		return logAndReturnError("Error stopping instance '"+instanceId+"': ", string(output))
 	}
 
-	log.Printf("Stopped VM '%s' successfully!", vmId)
+	log.Printf("Stopped instance '%s' successfully!", instanceId)
 
 	return nil
 }
 
-func (agent *ServerAgentImpl) RestartVM(vmId string) error {
-	log.Printf("Restarting VM '%s'...", vmId)
+// TODO: Implement
+func (agent *ServerAgentImpl) RestartInstance(instanceId string) error {
+	log.Printf("Restarting instance '%s'...", instanceId)
 
 	cmd := exec.Command(
-		"virsh", "reboot", vmId,
+		"virsh", "reboot", instanceId,
 	)
 
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
-		return logAndReturnError("Error restarting VM '"+vmId+"': ", string(output))
+		return logAndReturnError("Error restarting instance '"+instanceId+"': ", string(output))
 	}
 
-	log.Printf("Restarted VM '%s' successfully!", vmId)
+	log.Printf("Restarted instance '%s' successfully!", instanceId)
 
 	return nil
 }
+
+/* Keeping this as a reference for future use
 
 func (agent *ServerAgentImpl) ForceStopVM(vmId string) error {
 	log.Printf("Force stopping VM '%s'...", vmId)
@@ -129,9 +219,9 @@ func (agent *ServerAgentImpl) ForceStopVM(vmId string) error {
 	log.Printf("Force stopped VM '%s' successfully!", vmId)
 
 	return nil
-}
+}*/
 
-func (agent *ServerAgentImpl) ListVMsStatus() ([]ListVMsStatusResponse, error) {
+func (agent *ServerAgentImpl) ListInstancesStatus() ([]ListInstancesStatusResponse, error) {
 	log.Printf("Getting VMs status...")
 
 	cmd := exec.Command(
@@ -161,13 +251,113 @@ func (agent *ServerAgentImpl) ListVMsStatus() ([]ListVMsStatusResponse, error) {
 		log.Printf("%s: %s", vmName, status)
 	}
 
-	return toVMsStatusResponse(vmStatusMap), nil
+	return toListInstancesStatusResponse(vmStatusMap), nil
 }
 
-func toVMsStatusResponse(vmStatusMap map[string]string) []ListVMsStatusResponse {
-	response := []ListVMsStatusResponse{}
+func toListBaseImagesResponse(fileNames []string) []ListBaseImagesResponse {
+	response := []ListBaseImagesResponse{}
+	response = append(response, ListBaseImagesResponse{FileNames: fileNames})
+	return response
+}
+
+func createDir(path string) error {
+	return os.MkdirAll(path, 0755)
+}
+
+func (agent *ServerAgentImpl) createTemplateMetadataFiles(
+	templatePath string,
+	request DefineTemplateRequest,
+) error {
+	cloudInitMetadata := CloudInitMetadata{
+		InstanceId:    request.TemplateId,
+		LocalHostname: "template-" + request.TemplateId,
+	}
+
+	// Create meta-data.yaml file from template
+	tmpl, err := template.ParseFS(templateFS, "templates/meta-data.yaml.tmpl")
+	if err != nil {
+		return logAndReturnError("Error parsing meta-data.yaml template: ", err.Error())
+	}
+
+	metaDataFile, err := os.Create(templatePath + "/meta-data.yaml")
+	if err != nil {
+		return logAndReturnError("Error creating meta-data.yaml file: ", err.Error())
+	}
+
+	if err := tmpl.Execute(metaDataFile, cloudInitMetadata); err != nil {
+		return logAndReturnError("Error executing meta-data.yaml template: ", err.Error())
+	}
+
+	metaDataFile.Close()
+
+	// Copy user-data.yaml file from the instance used to create the template,
+	// this is because the template shouldn't need to configure new user-data,
+	// as they will never be started.
+	instancePath := agent.vmsStoragePath + "/" + request.SourceInstanceId
+	copyCmd := exec.Command(
+		"cp",
+		instancePath+"/user-data.yaml",
+		templatePath+"/user-data.yaml",
+	)
+
+	copyCmdOutput, err := copyCmd.CombinedOutput()
+	if err != nil {
+		return logAndReturnError("Error copying user-data.yaml file: ", string(copyCmdOutput))
+	}
+
+	// Create cidata.iso using genisoimage
+	createIsoCmd := exec.Command(
+		"genisoimage",
+		"-output", templatePath+"/cidata.iso",
+		"-V", "cidata",
+		"-r",
+		"-J", templatePath+"/meta-data.yaml", templatePath+"/user-data.yaml",
+	)
+
+	createIsoCmdOutput, err := createIsoCmd.CombinedOutput()
+
+	if err != nil {
+		return logAndReturnError("Error creating cidata.iso: ", string(createIsoCmdOutput))
+	}
+
+	return nil
+}
+
+func (agent *ServerAgentImpl) createDiskImage(path string, data CreateDiskImageData) error {
+	var sourceVmPath string
+	if data.sourceIsBase {
+		sourceVmPath = agent.vmsStoragePath + "/" + agent.cloudInitImagesPath + "/" + data.sourceVmId + ".qcow2"
+	} else {
+		sourceVmPath = agent.vmsStoragePath + "/" + data.sourceVmId + "/" + data.sourceVmId + ".qcow2"
+	}
+
+	createDiskImageCmd := exec.Command(
+		"qemu-img",
+		"create",
+		"-b", sourceVmPath,
+		"-f", "qcow2",
+		"-F", "qcow2",
+		path+"/"+data.sourceVmId+".qcow2",
+		strconv.Itoa(data.SizeMB)+"M",
+	)
+
+	createDiskImageCmdOutput, err := createDiskImageCmd.CombinedOutput()
+	if err != nil {
+		return logAndReturnError("Error creating disk image: ", string(createDiskImageCmdOutput))
+	}
+
+	return nil
+}
+
+func (agent *ServerAgentImpl) createInstanceMetadataFile(path string, request CreateInstanceRequest) error {
+	// TODO: Implement
+	return nil
+}
+
+func toListInstancesStatusResponse(vmStatusMap map[string]string) []ListInstancesStatusResponse {
+	response := []ListInstancesStatusResponse{}
 	for vmName, status := range vmStatusMap {
-		response = append(response, ListVMsStatusResponse{VmId: vmName, Status: status})
+		response = append(response, ListInstancesStatusResponse{InstanceId: vmName, Status: status})
 	}
 	return response
 }
@@ -181,6 +371,9 @@ func logAndReturnError(customMsg string, err string) error {
 	return fmt.Errorf("%s%s", customMsg, err)
 }
 
-func NewServerAgent() ServerAgent {
-	return &ServerAgentImpl{}
+func NewServerAgent(vmsStoragePath string, cloudInitImagesPath string) ServerAgent {
+	return &ServerAgentImpl{
+		vmsStoragePath:      vmsStoragePath,
+		cloudInitImagesPath: cloudInitImagesPath,
+	}
 }
