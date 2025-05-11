0a4a68f579f3db0a625668c42d9c2bf997cc23e4
Julen Rodriguez <122809250+JulenRM10@users.noreply.github.com>
2025-05-12 01:15:42 +0200

    feat: enhance instance creation process with UUID parsing for IDs; improve error logging in CreateInstance method; update API endpoint paths for instance creation and deletion

--- web-server/frontend/src/utils/Env.ts
+++ web-server/frontend/src/utils/Env.ts
@@ -9,10 +9,10 @@ export const getEnv = () => {
     API_VALIDATE_USER: `${VITE_API_URL}/users/validate`,
     API_BASES: `${VITE_API_URL}/bases`,
     API_ENROLL_USER_IN_SUBJECT: `${VITE_API_URL}/subjects/{subjectId}/add/users/{userEmail}`,
-    API_CREATE_INSTANCE: `${VITE_API_URL}/instances`,
+    API_CREATE_INSTANCE: `${VITE_API_URL}/instances/create`,
     API_CREATE_TEMPLATE: `${VITE_API_URL}/templates`,
     API_DELETE_SUBJECT: `${VITE_API_URL}/subjects/{id}`,
-    API_DELETE_INSTANCE: `${VITE_API_URL}/instances/{instanceId}`,
+    API_DELETE_INSTANCE: `${VITE_API_URL}/instances/delete/{instanceId}`,
     __vite__: otherViteConfig,
   }
 }
